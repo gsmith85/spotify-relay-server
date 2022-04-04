055cfa0b02b7a6ff15bee1468b5c900de209c967
Greg Smith <gregoryryansmith@gmail.com>
2022-04-04 09:47:51 -0700

    Clearer parameter naming on fetch #then lambdas

--- spotify.mjs
+++ spotify.mjs
@@ -1,26 +1,26 @@
-"use strict";
+'use strict';
 
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
 const ENCODED_CLIENT_AUTHENTICATION =
   (function base64EncodeClientIdAndSecret() {
     return new Buffer(
-      process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
-    ).toString("base64");
+      process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET
+    ).toString('base64');
   })();
 
 const SPOTIFY_AUTHORIZATION_TOKEN_PROMISE = (function getAuthorizationToken() {
   // https://developer.spotify.com/documentation/general/guides/authorization/client-credentials/
   const formParameters = new URLSearchParams();
-  formParameters.append("grant_type", "client_credentials");
+  formParameters.append('grant_type', 'client_credentials');
 
-  return fetch("https://accounts.spotify.com/api/token", {
-    method: "post",
+  return fetch('https://accounts.spotify.com/api/token', {
+    method: 'post',
     headers: {
-      Authorization: "Basic " + ENCODED_CLIENT_AUTHENTICATION,
+      Authorization: 'Basic ' + ENCODED_CLIENT_AUTHENTICATION,
     },
     body: formParameters,
-  }).then((j) => j.json());
+  }).then((response) => response.json());
 })();
 
 function makeRequestAfterAuthenticationTokenResolves(url) {
@@ -29,15 +29,15 @@ function makeRequestAfterAuthenticationTokenResolves(url) {
   ) {
     return fetch(url, {
       headers: {
-        Authorization: "Bearer " + authorizationTokenData.access_token,
+        Authorization: 'Bearer ' + authorizationTokenData.access_token,
       },
-    }).then((j) => j.json());
+    }).then((response) => response.json());
   });
 }
 
 function getPlaylist() {
   return makeRequestAfterAuthenticationTokenResolves(
-    "https://api.spotify.com/v1/playlists/" + process.env.PLAYLIST_ID
+    'https://api.spotify.com/v1/playlists/' + process.env.PLAYLIST_ID
   );
 }
 
